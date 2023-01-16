d547edb462bf144ef452af121709be5031252d1a
maxt0d0nt <maxbox21@hotmail.com>
2023-01-16 22:47:52 +0100

    navbar 24145

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import { Box } from '@mui/material'
+import { Box } from '@mui/material';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import ExerciseDetails from './pages/ExerciseDetails';
+import Footer from './components/Footer';
 
 const App = () => {
     return (
-        <Box width="400px">App
-            Navbar
+        <Box width="400px" sx={{ width: { xl: '1488px'}}} m='auto' >
+            <Navbar />
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/exercise/:id" element={<exerciseDetails />} />
+                <Route path="/" element={<Home />} exact/>
+                <Route path="/exercise/:id" element={<ExerciseDetails />} />
             </Routes>
+            <Footer />
         </Box>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Stack } from '@mui/material';
+import Logo from '../assets/images/Logo.png'
 
-export const Navbar = () => {
+const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Stack>
+      <Link to="/">
+        <img src={Logo} alt="logo gym"
+          style={{
+            width: '48px',
+            height: '48px',
+            margin: '0 20px'
+          }} />
+      </Link>
+      <Stack>
+        <Link to="/" style={{ textDecoration: 'none', color: '#ff2625'}} > HOME </Link>
+        <a href='#exercises' style={{
+          testDecoration: 'none', color: '#3a1212'
+        }}>EXERCISES</a>
+      </Stack>
+    </Stack>
   )
 }
+
+export default Navbar
