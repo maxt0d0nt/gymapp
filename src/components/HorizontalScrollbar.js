00c4bd207e842d21bdb170d76ba403d8f8b51b15
maxt0d0nt <maxbox21@hotmail.com>
2023-01-27 11:59:40 +0100

    scroll ojo

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -9,8 +9,8 @@ const LeftArrow = () => {
   const { scrollPrev } = useContext(VisibilityContext);
 
   return (
-    <Typography onClick= {() => scrollPrev()} className="left-arrow">
-      <img src={LeftArrowIcon} alt="left-arrow"/>
+    <Typography onClick={() => scrollPrev()} className="left-arrow">
+      <img src={LeftArrowIcon} alt="left-arrow" />
     </Typography>
   )
 }
@@ -19,28 +19,31 @@ const RightArrow = () => {
   const { scrollNext } = useContext(VisibilityContext);
 
   return (
-    <Typography onClick= {() => scrollNext()} className="right-arrow">
-      <img src={RightArrowIcon} alt="right-arrow"/>
+    <Typography onClick={() => scrollNext()} className="right-arrow">
+      <img src={RightArrowIcon} alt="right-arrow" />
     </Typography>
-  )}
+  )
+}
 
 const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
-  
+
   return (
-    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+    
+    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} className="aja">
       {data.map((item) => (
-      <Box
-      key={item.id || item}
-      itemId={item.id || item}
-        title={item.id || item}
-        m="0 40px"
- >
-<SecondaryMuscles item={item}
-          bodyPart={bodyPart}
-          setBodyPart={setBodyPart} />
-      </Box>)
+        <Box
+          key={item.id || item}
+          itemId={item.id || item}
+          title={item.id || item}
+          m="20px 40px"
+        >
+          <SecondaryMuscles item={item}
+            bodyPart={bodyPart}
+            setBodyPart={setBodyPart} />
+        </Box>)
       )}
     </ScrollMenu>
+    
   )
 }
 
