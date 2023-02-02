5e75fb202be530cf7b7a63063f0d748ed07d527d
maxt0d0nt <maxbox21@hotmail.com>
2023-02-02 11:16:32 +0100

    exercises pagination

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Stack, Typography } from '@mui/material';
+import { exercisesOptions, fetchData } from '../pages/utils/fetchData';
 
-const ExerciseCard = ({exercises}) => {
+const ExerciseCard = ({ exercises }) => {
+
   return (
+
     <Link className='exercise-card' >
-   <img src={exercises.gifUrl} alt={exercises.name} loading="lazy" />
+      <img src={exercises.gifUrl} alt={exercises.name} width="200px" loading="lazy" />
+      <Stack direction="row">
+        <Button sx={{
+          ml: '21px', color: '#fff', background: '#ffa9a9',
+    fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize'
+        }}>
+          {exercises.bodyPart}
+        </Button>
+        <Button sx={{
+          ml: '21px', color: '#fff', background: '#fcc757',
+    fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize'
+        }}>
+          {exercises.target}
+        </Button>
+      </Stack>
+      <Typography ml='21px' color='#000' fontWeight='bold' mt='11px' pb='10px' textTransform='capitalize'
+        fontSize='22px'>
+        {exercises.name}
+      </Typography>
     </Link>
   )
 }
--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,10 +1,22 @@
-import { Typography, Stack, Box } from '@mui/material';
-import React from 'react';
+import { Typography, Stack, Box, Pagination } from '@mui/material';
+import React, { useState } from 'react';
 import { exercisesOptions, fetchData } from '../pages/utils/fetchData';
 import ExerciseCard from './ExerciseCard';
 
 const Exercises = ({ exercises, setExercises, bodyPart}) => {
- 
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const exercisesPerPAge = 9;
+
+  const indexLastExercise = currentPage * exercisesPerPAge;
+  const indexOfFirtExercise = indexLastExercise - exercisesPerPAge;
+  const currentExercise = exercises.slice (indexOfFirtExercise. indexLastExercise)
+  
+  const paginate = (e, value) => {
+    setCurrentPage(value);
+
+    window.scrollTo({ top: 1800, behavior: ' smooth'})
+  }
   return (
     <Box id="exercises"
     sx={{mt: { lg: '110px'}}}
@@ -14,11 +26,25 @@ const Exercises = ({ exercises, setExercises, bodyPart}) => {
 <Typography variant='h3' mb="46px">
   Resultados
 </Typography>
-<Stack >
-  { exercises.map((exercises, index) => (
+<Stack direction="row" sx={{ gap: { lg: '110px', xs: '50px'}}} flexWrap="wrap" justifyContent="center">
+  { currentExercise.map((exercises, index) => (
     <ExerciseCard key={index} exercises={exercises} />
   ))}
 </Stack>
+<Stack mt='100px' alignItems='center'>
+  {exercises.length > 9 && (
+    <Pagination
+    color='standard'
+    shape='rounded'
+    defaultPage={1}
+    count={Math.ceil(exercises.length / exercisesPerPAge)}
+    page={currentPage}
+    onChange={paginate}
+    size='large'
+    />
+  )}
+</Stack>
+
     </Box>
   )
   
